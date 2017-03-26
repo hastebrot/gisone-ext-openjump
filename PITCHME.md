6a619f2c85c175d2c839fb3c15cb7ad5a5485ab2
Benjamin Gudehus <hastebrot@gmail.com>
2017-03-26 10:25:54 +0200

    (feat) Add sample code für XML binding (JAXB) and XML processing (JAXP).

--- PITCHME.md
+++ PITCHME.md
@@ -1,3 +1,47 @@
 #HSLIDE
 
-# gisone-ext-openjump
+`gisone-ext-openjump`
+
+#VSLIDE
+
+~~~xml
+<?xml version="1.0" encoding="UTF-8"?>
+<project>
+  <properties class="java.util.HashMap">
+    <mapping>
+      <key class="javax.xml.namespace.QName">Project File</key>
+      <value class="java.lang.String">/home/user/work/project-zero-layers.jmp</value>
+    </mapping>
+  </properties>
+  <windowLocation X="0" Y="0" />
+  <windowSize width="1160" height="871" />
+  <windowZoom left="0.0" right="1652.0" bottom="0.0" top="952.0" />
+  <maximized>true</maximized>
+  <layers>
+    <category name="Working" />
+    <category name="System" />
+  </layers>
+</project>
+~~~
+
+#VSLIDE
+
+~~~
+INFO: Found project.
+INFO: Found property "Project File".
+INFO: Found category "Working".
+INFO: Found category "System".
+~~~
+
+#VSLIDE
+
+elements:
+- `<project>`
+- `<properties>`, `<key>`, `<value>`
+- `<layers>`, `<category>`
+
+attributes:
+- `class="java.util.HashMap"`
+- `class="javax.xml.namespace.QName"`
+- `class="java.lang.String"`
+
--- projects/core/src/gisone/ext/openjump/domDocument.kt
+++ projects/core/src/gisone/ext/openjump/domDocument.kt
@@ -7,9 +7,21 @@ import kotlinx.dom.createDocument
 import kotlinx.dom.elements
 import kotlinx.dom.toXmlString
 import org.w3c.dom.Document
+import javax.xml.bind.JAXBContext
+import javax.xml.bind.annotation.XmlAccessType
+import javax.xml.bind.annotation.XmlAccessorType
+import javax.xml.bind.annotation.XmlAttribute
+import javax.xml.bind.annotation.XmlElement
+import javax.xml.bind.annotation.XmlRootElement
+import javax.xml.bind.annotation.XmlValue
 import javax.xml.transform.OutputKeys
 
 fun main(args: Array<String>) {
+    val xml = sampleJavaXmlProcessing()
+    sampleJavaXmlBinding(xml)
+}
+
+fun sampleJavaXmlProcessing(): String {
     val doc = createDocument()
 
     doc.addElement("customer") {
@@ -30,11 +42,45 @@ fun main(args: Array<String>) {
         }
     }
 
-    println(doc.toPrettyXmlString())
+    val xml = doc.toPrettyXmlString()
+    println(xml)
+
     println(doc.elements("customer")[0].getAttribute("first-name"))
     println(doc.elements("phone-number").map { it.children()[0].textContent })
     println(doc.elements("street")[0].textContent)
+//    println(doc.elements("phone-number")[0].toXmlString())
+    return xml
 }
+
+fun sampleJavaXmlBinding(xml: String) {
+    val unmarshaller = JAXBContext.newInstance(Customer::class.java)
+        .createUnmarshaller()
+    val customer = unmarshaller.unmarshal(xml.reader()) as Customer
+    println(customer)
+    println(customer.address)
+    println(customer.address?.phoneNumbers)
+}
+
+@XmlRootElement
+data class Customer(
+    @XmlAttribute(name = "first-name") val firstName: String? = null,
+    @XmlAttribute(name = "last-name") val lastName: String? = null
+) {
+    @XmlElement val address: Address? = null
+}
+
+@XmlAccessorType(XmlAccessType.FIELD)
+data class Address(
+    @XmlElement val street: String? = null
+) {
+    @XmlElement(name = "phone-number") val phoneNumbers = mutableListOf<PhoneNumber>()
+}
+
+@XmlAccessorType(XmlAccessType.FIELD)
+data class PhoneNumber(
+    @XmlAttribute val type: String? = null,
+    @XmlValue val value: String? = null
+)
 
 private object OutputKeysExt {
     val INDENT_AMOUNT = "{http://xml.apache.org/xslt}indent-amount"
